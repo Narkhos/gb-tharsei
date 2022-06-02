d81f2f59184d6f50e5effa3f55ea3d0fc579933c
Xavier Direz <xavierdirez@hotmail.com>
2022-06-02 22:52:55 +0200

    feat: credit english

--- src/credits.c
+++ src/credits.c
@@ -7,24 +7,43 @@
 
 // DRAW CREDITS
 void draw_credits(unsigned int x, unsigned int y) {
-	text_print_string_bkg(x, y,      "                    ");
-	text_print_string_bkg(x, y +  1, "                    ");
-	text_print_string_bkg(x, y +  2, "      THARSEII      ");
-	text_print_string_bkg(x, y +  3, "                    ");
-	text_print_string_bkg(x, y +  4, "                    ");
-	text_print_string_bkg(x, y +  5, "                    ");
-	text_print_string_bkg(x, y +  6, "                    ");
-	text_print_string_bkg(x, y +  7, "                    ");
-	text_print_string_bkg(x, y +  8, "                    ");
-	text_print_string_bkg(x, y +  9, "  PAR XAVIER DIREZ  ");
-	text_print_string_bkg(x, y + 10, "     2019-2022      ");
-	text_print_string_bkg(x, y + 11, "                    ");
+	if (english) {
+		text_print_string_bkg(x, y,      "                    ");
+		text_print_string_bkg(x, y +  1, "                    ");
+		text_print_string_bkg(x, y +  2, "      THARSEII      ");
+		text_print_string_bkg(x, y +  3, "                    ");
+		text_print_string_bkg(x, y +  4, "                    ");
+		text_print_string_bkg(x, y +  5, "                    ");
+		text_print_string_bkg(x, y +  6, "                    ");
+		text_print_string_bkg(x, y +  7, "                    ");
+		text_print_string_bkg(x, y +  8, "                    ");
+		text_print_string_bkg(x, y +  9, "  BY  XAVIER DIREZ  ");
+		text_print_string_bkg(x, y + 10, "     2019-2022      ");
+		text_print_string_bkg(x, y + 11, "                    ");
+	} else {
+		text_print_string_bkg(x, y,      "                    ");
+		text_print_string_bkg(x, y +  1, "                    ");
+		text_print_string_bkg(x, y +  2, "      THARSEII      ");
+		text_print_string_bkg(x, y +  3, "                    ");
+		text_print_string_bkg(x, y +  4, "                    ");
+		text_print_string_bkg(x, y +  5, "                    ");
+		text_print_string_bkg(x, y +  6, "                    ");
+		text_print_string_bkg(x, y +  7, "                    ");
+		text_print_string_bkg(x, y +  8, "                    ");
+		text_print_string_bkg(x, y +  9, "  PAR XAVIER DIREZ  ");
+		text_print_string_bkg(x, y + 10, "     2019-2022      ");
+		text_print_string_bkg(x, y + 11, "                    ");
+	}
 }
 
 UINT8 state_credits() {
 	INT8 selected = -1;
 
-	text_print_string_win(1, 2, "RETOUR");
+	if (english) {
+		text_print_string_win(1, 2, "BACK");
+	} else {
+		text_print_string_win(1, 2, "RETOUR");
+	}
 
 	selected = draw_menu();
 
